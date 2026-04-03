e6966194a699e4cdb01d1abdbf6330163729a41e
Moningi Nidhi <nidhimoningi2556@gmail.com>
2026-04-03 13:29:51 +0530

    Added movie details and favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [favorites, setFavorites] = useState([]);
 
   const searchMovies = async () => {
     setLoading(true);
@@ -27,40 +30,53 @@ function App() {
   };
 
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>🎬 Movie Search App</h1>
+    <div className="grid">
+      {movies.map((movie) => (
+        <div
+          key={movie.imdbID}
+          className="card"
+          onClick={() => getMovieDetails(movie.imdbID)}
+        >
+          <img
+            src={
+              movie.Poster !== "N/A"
+                ? movie.Poster
+                : "https://via.placeholder.com/200x300?text=No+Image"
+            }
+            alt={movie.Title}
+          />
 
-      <input
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+          <h3>{movie.Title}</h3>
+          <p>{movie.Year}</p>
 
-      <button onClick={searchMovies}>Search</button>
-
-      {loading && <p>Loading...</p>}
-      {!loading && movies.length === 0 && <p>No movies found</p>}
-
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "20px", marginTop: "20px" }}>
-        {movies.map((movie) => (
-          <div key={movie.imdbID} style={{ width: "200px" }}>
-            <img
-              src={
-                movie.Poster !== "N/A"
-                  ? movie.Poster
-                  : "https://via.placeholder.com/200x300?text=No+Image"
-              }
-              alt={movie.Title}
-              width="100%"
-            />
-            <h3>{movie.Title}</h3>
-            <p>{movie.Year}</p>
-          </div>
-        ))}
-      </div>
+          <button
+            onClick={(e) => {
+              e.stopPropagation(); // prevents card click
+              addFavorite(movie);
+            }}
+          >
+            ❤️
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
+
+const getMovieDetails = async (id) => {
+  try {
+    const res = await fetch(
+      `https://movie-backend-xew0.onrender.com/movie/${id}`
+    );
+    const data = await res.json();
+    setSelectedMovie(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const addFavorite = (movie) => {
+  setFavorites([...favorites, movie]);
+};
 
 export default App;
